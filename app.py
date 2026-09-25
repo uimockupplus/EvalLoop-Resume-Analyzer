3881c145efc98c2de64af3b9f3941fd14185db04
uimockupplus <techultron2020@gmail.com>
2026-09-25 12:42:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-
+from pydf import PdfReader
+pdf_path = "resumes/my_resume.pdf"
+reader = PdfReader(pdf_path)
+text=" "
+for page in reader.pages:
+  text += pages.extract_text() or " "
+  print("______Resume Text_______")
+  print (text)
